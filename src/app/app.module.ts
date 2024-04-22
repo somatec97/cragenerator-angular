6cb947e51d55658c251319509374e72e9407dfd1
BEN BAHA <soumia.azza.91.97@gmail.com>
2024-04-22 17:38:37 +0200

    (CG-5):Angular material est mis en place sur la partie front avec des modification de code sur la partie back Spring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input'; 
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select'; 
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -14,6 +23,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule, HttpClientModule,
     AppRoutingModule,
     MatSlideToggleModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatSelectModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule
   ],
   providers: [],
