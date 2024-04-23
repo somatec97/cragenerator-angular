039bfeacc10a3bf78ee7b1fed1ffc03ea11adf51
BEN BAHA <soumia.azza.91.97@gmail.com>
2024-04-23 17:58:54 +0200

    confli git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,47 @@
 import { Component } from '@angular/core';
 import { CrageneratorService } from './cragenerator.service';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { OnInit } from '@angular/core';
+import { FormArray, FormBuilder, Validators, FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   providers: [CrageneratorService]
 })
-export class AppComponent  {
+export class AppComponent  implements OnInit {
   title = 'cragenerator';
   pdf = Object
-  craForm : any;
+  craForm !: FormGroup;
 
-  constructor(private crageneratorService: CrageneratorService){
-    this.craForm = {};
-
-  }
- 
+  constructor(private crageneratorService: CrageneratorService, private formBuilder: FormBuilder){}
+    ngOnInit(){
+      this.craForm = this.formBuilder.group({
+        description:"",
+        tjm:0.0,
+        lignes: this.formBuilder.array([
+          this.creerLigne()
+        ])
+      });
+    }
+    
+    creerLigne(): FormGroup{
+      return this.formBuilder.group({
+        date:null,
+        heuresTravail:0,
+      });
+    }
+    ajoutLigne(): void{
+      this.lignes.push(this.creerLigne());
+    }
+    get lignes(): FormArray{
+      return this.craForm.get('lignes') as FormArray;
+    }
+    
   genererCraPdf(craForm: any){
-    this.crageneratorService.genererCraPdf(this.craForm).subscribe(x => {
+    
+    console.log(this.craForm.value);
+    this.crageneratorService.genererCraPdf(this.craForm.value).subscribe(x => {
       const blob = new Blob([x], {type: 'app/pdf'});
       const data = window.URL.createObjectURL(blob);
       const link = document.createElement('a');
