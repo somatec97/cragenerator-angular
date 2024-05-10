1051ed22af7ae2d86b8759cfd650f8dbe04c53e6
BEN BAHA <soumia.azza.91.97@gmail.com>
2024-05-10 15:54:41 +0200

    (CG-12):environnements

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+import { ApiJFService } from "src/app/api-jf.service";
+
+export const environment = {
+    production: false,
+    crageneratorApiUrl: 'http://localhost:8080',
+    apiJFApiUrl: 'https://calendrier.api.gouv.fr/jours-feries/metropole/'
+};
